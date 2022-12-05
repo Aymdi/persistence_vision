0f99be644cc2b38312e61019ca9f89b5d68169c5
LAMHAMDI Aymane <Aymane.Lamhamdi@bordeaux-inp.fr>
2022-12-05 09:05:20 +0100

    adding com bluetooth

---
+++ com_blue.c
@@ -0,0 +1,55 @@
+#define F_CPU 13000000UL // Clock Speed
+#define BAUD 38400
+#define MYUBRR F_CPU/16/BAUD-1
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+char data;
+char data2 = 'b';
+
+void USART_Init(unsigned int ubrr)
+{
+    /* Set baud rate */
+    UBRR0H = (unsigned char)(ubrr>>8);
+    UBRR0L = (unsigned char)(ubrr);
+    
+    /* Enable receiver and transmitter */
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+    
+    /* Set frame format: 8data, 2stop bit */
+    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+char USART_Receive(void)
+{
+    /* Wait for data to be received */
+    while ( !(UCSR0A & (1<<RXC0)) );
+    /* Get and return received data from buffer */
+    return UDR0;
+}
+
+void USART_Transmit(char data)
+{
+    /* Wait for empty transmit buffer */
+    while ( !( UCSR0A & (1<<UDRE0)) );
+    /* Put data into buffer, sends the data */
+    UDR0 = data;
+}
+
+int main(void)
+{
+    DDRB  = 0b00000001;
+    PORTB = 0b00000000;
+    USART_Init(MYUBRR);
+    while (1) {
+        data = USART_Receive();
+        USART_Transmit(data2);
+        if (data == 'a') {
+            PORTB = 0b00000001;
+        } else if (data == 'c') {
+            PORTB = 0b00000000;
+        }
+    }
+}
