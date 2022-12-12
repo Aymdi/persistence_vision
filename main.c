11c5a5cd228a87d06536cf8f1e1531f383362e12
LAMHAMDI Aymane <Aymane.Lamhamdi@bordeaux-inp.fr>
2022-12-12 11:24:34 +0100

    up timer

--- main.c
+++ main.c
@@ -2,17 +2,38 @@
 #include <util/delay.h>
 #include "effet_Hall.h"
 #include "com_blue.h"
+#include <avr/interrupt.h>
+
+struct RingBuffer buffer;
 
 void main() {
-    struct RingBuffer buffer;
-    DDRB  = 0b00000001;
-    PORTB = 0b00000000;
-    USART_Init(MYUBRR);
-    ringBufferInit(&buffer);
-    while (1) {
-        ringBufferWrite(&buffer, USART_Receive());
-        if (ringBufferAvailable(&buffer)){
-            USART_Transmit(ringBufferRead(&buffer));
+    DDRB = 0x01;
+	
+	TCCR1B = (1 << WGM12);
+	OCR1A = 19531;
+	TIMSK1 = (1 << OCIE1A);
+	
+	sei();
+	
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+	
+    while(1)
+    {
+        //code d'application
+        DDRB  = 0b00000001;
+        PORTB = 0b00000000;
+        USART_Init(MYUBRR);
+        ringBufferInit(&buffer);
+        while (1) {
+            ringBufferWrite(&buffer, USART_Receive());
+            if (ringBufferAvailable(&buffer)){
+                USART_Transmit(ringBufferRead(&buffer));
+            }
         }
     }
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	PORTB ^= (1 << PORTB0);
 }
